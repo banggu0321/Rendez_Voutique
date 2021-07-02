2d2e02a347079ee4d7f79421535997d46426ff9c
https://github.com/banggu0321/Rendez_Voutique.git <99bang@naver.com>
2021-07-02 23:17:40 +0900

    final

---
+++ js/brand.js
@@ -0,0 +1,25 @@
+/* 드롭다운 */
+$(function(){
+    $('.menulink').hover(function(){
+        $('.b_menu').css({'display' : 'block'});
+        $('.menulink').css({'overflow' : 'visible'});
+    });
+    $('.menulink').mouseleave(function(){
+        $('.b_menu').css('display', 'none');
+        $('.menulink').css({'overflow' : 'hidden'});
+    });
+    $('.b_menu').hover(function(){
+        $('.b_menu').css({'display' : 'block'});
+        $('.menulink > li > ul').css({'opacity' : '100%'});
+        $('.menulink').css({'overflow' : 'visible'});
+    });
+    $('.b_menu').mouseleave(function(){
+        $('.b_menu').css('display', 'none');
+        $('.menulink').css({'overflow' : 'hidden'});
+    });
+    $('.menu_name > ul').hover(function(){
+        $('.b_menu').css({'display' : 'block'});
+        $('.menulink > li > ul').css({'opacity' : '100%'});
+        $('.menulink').css({'overflow' : 'visible'});
+    });
+});
